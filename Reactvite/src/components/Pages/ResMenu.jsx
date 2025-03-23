6c56d657666b3fc4713866dcd948d863cfc6e25b
Ishpreet Singh <ishpreet@outlook.in>
2025-03-24 02:27:42 +0530

    mapitems fixed

--- Reactvite/src/components/Pages/ResMenu.jsx
+++ Reactvite/src/components/Pages/ResMenu.jsx
@@ -15,9 +15,17 @@ const Resmenu = () => {
     const name  = resInfo?.data?.cards[2]?.card?.card?.info.name;
     const cuisines  = resInfo?.data?.cards[2]?.card?.card?.info.cuisines.join(",");
     const costfortwo = resInfo?.data?.cards[2]?.card?.card?.info.costForTwoMessage;
-    const itemCards = resInfo?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards[0]?.card?.info;
+    const itemCards = resInfo?.data.cards[4].groupedCard.cardGroupMap.REGULAR.cards[1].card.card.itemCards;
     const itemprice = resInfo?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards[0]?.card?.info.price;
+    console.log("here");
     if (resInfo===null) return <Shimmer/>;
+
+    const meow = itemCards.map((bo) => {
+        return bo.card.info.name
+    })
+    
+    console.log({meow})
+
     return (
         <div className="menu">
             <h1>{name}</h1>
@@ -25,9 +33,9 @@ const Resmenu = () => {
             {/* <h1>{resInfo?.data.cards[1].card.card.gridElements?.infoWithStyle.restaurants.cuisins}</h1> */}
             <h2>Menu</h2>
 
-            {/* <h2>{itemname + " Price : "+ "₹"+itemprice/100}</h2> */}
+            {/* <h2>{itemCards.name + " Price : "+ "₹"+itemprice/100}</h2> */}
             <ul>
-                {/* <li>{itemCards.map((item) => <li>{item?.name}</li> )}</li> */}
+                <li>{meow.map((item) => <li>{item}</li> )}</li>
             </ul>
         </div>
     )
