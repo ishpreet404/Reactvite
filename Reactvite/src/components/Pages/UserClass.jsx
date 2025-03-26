99d1ff1860a23fb7ba2de8044fefd9e412caac2a
Ishpreet Singh <ishpreet@outlook.in>
2025-03-27 00:35:55 +0530

    setState in class based components

--- Reactvite/src/components/Pages/UserClass.jsx
+++ Reactvite/src/components/Pages/UserClass.jsx
@@ -4,11 +4,25 @@ import React from "react";
 class UserClass extends React.Component {
     constructor(props) {
         super(props)
+        this.state={  //state variable 
+            count :0,
+        }
     }
     render() {
         const { name } = this.props;
         return (
             <div className="user-card">
+                <h1>Count :{this.state.count}</h1>
+                <button onClick={() => {
+                    this.setState({
+                        count: this.state.count + 1,
+                    })
+                }}>increment count </button>
+                <button onClick={() => {
+                    this.setState({
+                        count: 0,
+                    })
+                }}>RESET  count </button>
                 <h1>Name : {name}</h1>
                 <h3>Location : Delhi</h3>
                 <h4>Contact: ishpreet@outlook.in</h4>
