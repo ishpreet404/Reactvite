d8095061e407fb8d661761f2b0cbf6e32182b7b7
Ishpreet Singh <ishpreet@outlook.in>
2025-03-27 19:10:02 +0530

    parent child react componentdidmount lifecycle

--- Reactvite/src/components/Pages/About.jsx
+++ Reactvite/src/components/Pages/About.jsx
@@ -3,10 +3,11 @@ import UserClass from "./UserClass";
 const Aboutus = () => {
   return (
     <div>
-      <h1>About Us</h1>
-      <h2>About us page</h2>
-    </div>,
-    <UserClass name="Ishpreet Singh"/>
+      {/* <h1>About Us</h1>
+      <h2>About us page</h2> */}
+    <UserClass name=" Ishpreet Singh"/>
+    {/* <UserClass name="Second"/> */}
+    </div>
   );
 };
 export default Aboutus;
--- Reactvite/src/components/Pages/UserClass.jsx
+++ Reactvite/src/components/Pages/UserClass.jsx
@@ -4,16 +4,24 @@ import React from "react";
 class UserClass extends React.Component {
     constructor(props) {
         super(props)
-        this.state={  //state variable 
-            count :0,
-        }
+        // this.state={  //state variable 
+        //     count: 0,
+            
+        // }
+        // console.log(this.props.name + "constructor called");
+    }
+    componentDidMount() {
+        console.log(this.props.name + "component did mount is used to call api ");
+        //because basic componets load hone ke baad api ki data aayega aur render ho jayega basically we will not have to wait for the data from api to render the webpage ui 
+        
     }
     render() {
+        console.log(this.props.name + "render called");
         const { name } = this.props;
         return (
             <div className="user-card">
-                <h1>Count :{this.state.count}</h1>
-                <button onClick={() => {
+                {/* <h1>Count :{this.state.count}</h1> */}
+                {/* <button onClick={() => {
                     this.setState({
                         count: this.state.count + 1,
                     })
@@ -22,7 +30,7 @@ class UserClass extends React.Component {
                     this.setState({
                         count: 0,
                     })
-                }}>RESET  count </button>
+                }}>RESET  count </button> */}
                 <h1>Name : {name}</h1>
                 <h3>Location : Delhi</h3>
                 <h4>Contact: ishpreet@outlook.in</h4>
